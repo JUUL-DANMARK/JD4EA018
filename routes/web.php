aec9cb64809aa91b8b5be871428949ffe83d4129
Christian Fredegaard <christian@fredegaard.com>
2024-09-24 02:49:17 +0200

    Enhanced routing with dynamic views for categories and courses

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,21 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Models\Category;
+use App\Models\Course;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'categories' => Category::all(),
+    ]);
 });
 
-/*Route::get('/{category}', function () {
-    return view('category');
-});
+Route::get('/{category}', [CategoryController::class, 'show'])->name('category');
 
-Route::get('/{category}/{course}', function () {
-    return view('course');
-});*/
+Route::get('/{category}/{course?}', function () {
+    return view('category.course.show', [
+        'category' => Category::where('slug', request()->segment(1))->firstOrFail(),
+        'course' => Course::where('slug', request()->segment(2))->firstOrFail(),
+    ]);
+})->where('category', '^[a-zA-Z0-9-_\/]+$');
